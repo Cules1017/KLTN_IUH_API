47041c44fea52bccaa06a804e1c1a558adedcc4b
hieunghia <nghianguyenhieu@vnpt.vn>
2024-01-14 23:42:13 +0700

    sua code ten bản Pú

--- database/migrations/2014_10_12_000000_create_noti_table.php
+++ database/migrations/2014_10_12_000000_create_noti_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reports', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
             $table->enum('type_id',[1,2]);
             $table->integer('client_id');
